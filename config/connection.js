b337bf7a70542ee121c7355e07da26469a53f737
Ricardo Sotolongo <rickey.soto@gmail.com>
2019-10-14 07:32:49 -0400

    added jaws_db .

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,18 @@
 //estabish access to mysql db via client side port, then export connection for dependencies
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-    port: 3306,
-    host: "localhost",
-    user: "root",
-    password: "rootroot",
-    database: "burgers_db"
-});
-
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        port: 3306,
+        host: "localhost",
+        user: "root",
+        password: "rootroot",
+        database: "burgers_db"
+    });
+}
 connection.connect(function (err) {
     if (err) {
         console.error("error connecting: " + err.stack);
